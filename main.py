50ca56166d6fa6592cc41ef5e5098f2fa1bbd3a9
shibainu1969 <tetsu.segi@gmail.com>
2018-08-24 15:41:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=random.choice(["ニャー！", "ニャーニャー！"])))
+        TextSendMessage(text=random.choice(["ニャー！", "ニャーニャー！", "ねむいニャ", "ねたニャ", "おなかがすいたニャ", "あそんでニャ"])))
 
 
 if __name__ == "__main__":
