c9d18944e41e8e8e3073dbfe022eb18df2fc89b6
SEGI Tetsu <segi@SEGInoMacBook-Pro.local>
2018-08-27 14:47:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=create_response(message)))
+        TextSendMessage(text=create_response(event.message.txt)))
 
 def create_response(message):
     return random.choice(["ニャー！", "ニャーニャー！", "ねむいニャ", "ねたニャ", "おなかがすいたニャ", "あそんでニャ", "チュール！ チュール！"])
