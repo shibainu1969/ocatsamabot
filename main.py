89256ee9cc9d3b9cf4a703c71cceb48d8e309e9e
shibainu1969 <tetsu.segi@gmail.com>
2018-08-29 10:11:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -123,7 +123,9 @@ def get_keyword(message):
     }
     url = Request(KEYWORD_URL, dumps(json_str).encode(), request_header)
     response = loads(urlopen(url).read())
-    print(response["result"][0]
+
+    print(response["result"][0])
+    
     if (len(response["result"]) == 0):
         return "キーワードなし"
     else:
